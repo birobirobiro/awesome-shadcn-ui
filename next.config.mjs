078a5212ff2023f1302c6a693227aa92d89898b2
João Inácio Neto <birobirobiro.dev@gmail.com>
2026-01-31 17:58:16 -0300

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,23 @@ const nextConfig = {
     removeConsole:
       process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "awesome-shadcn-ui.vercel.app",
+          },
+        ],
+        destination:
+          "https://awesome-shadcn-ui.birobirobiro-dev.workers.dev/:path*",
+        permanent: true, // 301
+      },
+    ];
+  },
 };
 
 export default nextConfig;
