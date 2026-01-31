dfd750de46384545b39afa0bde0183e397978fd3
João Inácio Neto <birobirobiro.dev@gmail.com>
2026-01-31 17:12:50 -0300

    remove cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
-});
+export default defineCloudflareConfig({});
