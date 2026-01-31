4088bc2b3b339e8cc00e3ff20f733c749f2340d7
João Inácio Neto <birobirobiro.dev@gmail.com>
2026-01-31 16:43:13 -0300

    updates

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({});
+export default defineCloudflareConfig({
+  incrementalCache: r2IncrementalCache,
+});
